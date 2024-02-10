8874547d285b20a4c6b9f6dd69f3ab230b6b91ba
Arjun Raj <arjunraj@One.local>
2024-02-10 11:54:42 +0530

    secondcommit

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:fooddon/welcome.dart';
-
 import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
